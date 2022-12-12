f94ba61e399a8ce68d72037bc144a5b0fda81bb7
raffaele.lubrano <raffaele.lubrano@InfoTele23.maxwell.local>
2022-12-12 09:28:19 +0100

    retta

---
+++ src/rettainterfacciagrafica/RettaInterfacciaGrafica.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rettainterfacciagrafica;
+
+/**
+ *
+ * @author raffaele.lubrano
+ */
+public class RettaInterfacciaGrafica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
